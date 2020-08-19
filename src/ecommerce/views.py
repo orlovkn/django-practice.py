4b85ac78fb4d383bd5bcaf354d4ab8e3e2835246
orlovkn <orlovkn.job@gmail.com>
2020-08-19 06:33:46 +0300

    wip: auth and register

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 
 from .forms import ContactForm, LoginForm, RegisterForm
 
@@ -9,6 +9,9 @@ def home_page(request):
     context = {
         "title": "main page"
     }
+    if request.user.is_authenticated:
+        context["premium_content"] = "some words"
+
     return render(request, "home_page.html", context)
 
 
@@ -61,7 +64,7 @@ def login_page(request):
 
     return render(request, "auth/login.html", context)
 
-
+User = get_user_model()
 def register_page(request):
     form = RegisterForm(request.POST or None)
     context = {
@@ -69,7 +72,15 @@ def register_page(request):
     }
     if form.is_valid():
         print(form.cleaned_data)
-        context['form'] = LoginForm()
+        # context['form'] = RegisterForm()
+
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+
+        user = User.objects.create_user(username, email, password)
+        print(user)
+
     return render(request, "auth/register.html", context)
 
 
