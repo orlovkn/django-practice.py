4f86f3809c543f33cc6f6e47616ef7807b46a30a
orlovkn <orlovkn.job@gmail.com>
2020-08-24 07:06:37 +0300

    wip

--- src/products/models.py
+++ src/products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10, default=0.1)
+
+    def __str__(self):
+        return self.title
